b99fdbfb2768177c7a50db87b8200907381d9db0
Alpha62579 <50741530+surya62579@users.noreply.github.com>
2022-01-21 15:27:45 +0530

    updater nonsense

--- AOUutilsHelper/build.gradle.kts
+++ AOUutilsHelper/build.gradle.kts
@@ -1,2 +1,9 @@
 version = "1.0.0"
 description = "This is a plugin to softban scammers.\nNote: This plugin is only intended for use with AOUutils and the All Of Us discord server."
+
+aliucord.changelog.set(
+        """
+        # 1.0.1
+        * Trigger update because I wanted to see the updater in action. :>
+        """.trimIndent()
+)
