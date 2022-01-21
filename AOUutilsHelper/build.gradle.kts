95c41e47dfba475d6342116cb9f61a07f5eaffc1
Alpha62579 <50741530+surya62579@users.noreply.github.com>
2022-01-21 15:33:12 +0530

    forgot to change the version number, updater didn't get triggered :<

--- AOUutilsHelper/build.gradle.kts
+++ AOUutilsHelper/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "This is a plugin to softban scammers.\nNote: This plugin is only intended for use with AOUutils and the All Of Us discord server."
 
 aliucord.changelog.set(
