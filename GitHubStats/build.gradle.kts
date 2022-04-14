4614e937cc0e5ce0f0a2b31963178d3d04ff07f1
Alpha62579 <50741530+Alpha62579@users.noreply.github.com>
2022-04-14 19:18:09 +0530

    GitHubStats v1.1.0 Fixed the wacky patching which caused the stats to either refuse to show up or only show up the next time you opened anyone's profile

--- GitHubStats/build.gradle.kts
+++ GitHubStats/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.0.2"
+version = "1.1.0"
 description = "Show a user's GitHub stats on their profile."
 
 aliucord.changelog.set(
@@ -10,5 +10,7 @@ aliucord.changelog.set(
             # 1.0.2
             * Fix something
             * This is the version where I question myself how I find bugs after publishing the code.
+            # 1.1.0
+            * Make things consistent, it usually broke due to wacky patching
         """.trimIndent()
 )
--- GitHubStats/src/main/java/com/alphapython/githubstats/GitHubStats.java
+++ GitHubStats/src/main/java/com/alphapython/githubstats/GitHubStats.java
@@ -13,10 +13,9 @@ import com.aliucord.Utils;
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.api.SettingsAPI;
 import com.aliucord.entities.Plugin;
-import com.aliucord.patcher.Hook;
+import com.aliucord.patcher.PreHook;
 import com.aliucord.utils.DimenUtils;
 import com.aliucord.utils.ReflectUtils;
-import com.discord.api.connectedaccounts.ConnectedAccount;
 import com.discord.databinding.WidgetUserSheetBinding;
 import com.discord.widgets.user.profile.UserProfileConnectionsView;
 import com.discord.widgets.user.usersheet.WidgetUserSheet;
@@ -25,6 +24,7 @@ import com.lytefast.flexinput.R;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
@@ -44,100 +44,101 @@ public class GitHubStats extends Plugin {
     @Override
     public void start(Context context) throws NoSuchMethodException {
         pluginsettings = settings;
-        AtomicReference<ArrayList<String>> statHolder = new AtomicReference<>();
 
-        patcher.patch(UserProfileConnectionsView.ViewHolder.class.getDeclaredMethod("onConfigure", int.class, UserProfileConnectionsView.ConnectedAccountItem.class), new Hook(methodHookParam -> {
-            // Get connected account
-            var account = (ConnectedAccount) ((UserProfileConnectionsView.ConnectedAccountItem) methodHookParam.args[1]).getConnectedAccount();
-            if (account.g().equals("github")) {  // ConnectedAccount.g() is the lowercase connection name
-                statHolder.set(getStats(account.d()));
-            }
-        }));
-
-        patcher.patch(WidgetUserSheet.class.getDeclaredMethod("configureGuildSection", WidgetUserSheetViewModel.ViewState.Loaded.class), new Hook(methodHookParam -> {
-            if (statHolder.get() == null) return;
-            var stats = statHolder.get();
-            statHolder.set(null);
+        patcher.patch(WidgetUserSheet.class.getDeclaredMethod("configureConnectionsSection", WidgetUserSheetViewModel.ViewState.Loaded.class), new PreHook(methodHookParam -> {
             var _this = (WidgetUserSheet) methodHookParam.thisObject;
-            try {
-                // Get binding and the user sheet
-                WidgetUserSheetBinding binding = (WidgetUserSheetBinding) ReflectUtils.invokeMethod(WidgetUserSheet.class, _this, "getBinding");
-                assert binding != null;
-                var sheetContent = (LinearLayout) binding.getRoot().findViewById(Utils.getResId("user_sheet_content", "id"));
-                var ctx = sheetContent.getContext();
+            var connectedAccountItems = ((WidgetUserSheetViewModel.ViewState.Loaded) methodHookParam.args[0]).getConnectionsViewState().getConnectedAccountItems();
+            for (UserProfileConnectionsView.ConnectedAccountItem item :
+                    connectedAccountItems) {
+                if (item.getConnectedAccount().g().equals("github")) {
+                    try {
+                        // Get stats
+                        var stats = getStats(item.getConnectedAccount().d());
 
-                // The view already exists, no need for duplicates.
-                if (sheetContent.findViewById(viewID) != null) return;
+                        // API yielded null
+                        if (stats == null) return;
 
-                // Define container for stats
-                LinearLayout layout = new LinearLayout(ctx);
-                layout.setId(viewID);
-                layout.setOrientation(LinearLayout.VERTICAL);
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                params.setMargins(DimenUtils.dpToPx(16), 0, DimenUtils.dpToPx(16), 0);
-                layout.setLayoutParams(params);
+                        // Get binding and the user sheet
+                        WidgetUserSheetBinding binding = (WidgetUserSheetBinding) ReflectUtils.invokeMethod(WidgetUserSheet.class, _this, "getBinding");
+                        assert binding != null;
+                        var sheetContent = (LinearLayout) binding.getRoot().findViewById(Utils.getResId("user_sheet_content", "id"));
+                        var ctx = sheetContent.getContext();
 
-                // Header
-                TextView header = new TextView(ctx, null, 0, R.i.UserProfile_Section_Header);
-                header.setText("GITHUB");
-                LinearLayout.LayoutParams viewParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                viewParams.setMargins(0, 0, 0, DimenUtils.dpToPx(16) / 2);
-                header.setLayoutParams(viewParams);
-                layout.addView(header);
+                        // The view already exists, yeet it
+                        if (sheetContent.findViewById(viewID) != null)
+                            sheetContent.removeView(sheetContent.findViewById(viewID));
 
-                var itemParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                itemParams.setMargins(DimenUtils.dpToPx(16), 0, 0, 0);
+                        // Define container for stats
+                        LinearLayout layout = new LinearLayout(ctx);
+                        layout.setId(viewID);
+                        layout.setOrientation(LinearLayout.VERTICAL);
+                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        params.setMargins(DimenUtils.dpToPx(16), 0, DimenUtils.dpToPx(16), 0);
+                        layout.setLayoutParams(params);
 
-                // Hardcode montage
-                // All the stats
-                if (pluginsettings.getBool("stars", true)) {
-                    TextView stars = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
-                    stars.setText("Total Stars Earned: " + stats.get(0));
-                    stars.setLayoutParams(itemParams);
-                    layout.addView(stars);
-                }
+                        // Header
+                        TextView header = new TextView(ctx, null, 0, R.i.UserProfile_Section_Header);
+                        header.setText("GITHUB");
+                        LinearLayout.LayoutParams viewParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        viewParams.setMargins(0, 0, 0, DimenUtils.dpToPx(16) / 2);
+                        header.setLayoutParams(viewParams);
+                        layout.addView(header);
+
+                        var itemParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        itemParams.setMargins(DimenUtils.dpToPx(16), 0, 0, 0);
+
+                        // Hardcode montage
+                        // All the stats
+                        if (pluginsettings.getBool("stars", true)) {
+                            TextView stars = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
+                            stars.setText("Total Stars Earned: " + stats.get(0));
+                            stars.setLayoutParams(itemParams);
+                            layout.addView(stars);
+                        }
 
 
-                if (pluginsettings.getBool("commits", true)) {
-                    TextView commits = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
-                    commits.setText("Total Commits" + (pluginsettings.getBool("yearly", true) ? String.format("(%s)", Calendar.getInstance().get(Calendar.YEAR)) : "") + ": " + stats.get(1));
-                    commits.setLayoutParams(itemParams);
-                    layout.addView(commits);
-                }
+                        if (pluginsettings.getBool("commits", true)) {
+                            TextView commits = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
+                            commits.setText("Total Commits" + (pluginsettings.getBool("yearly", true) ? String.format("(%s)", Calendar.getInstance().get(Calendar.YEAR)) : "") + ": " + stats.get(1));
+                            commits.setLayoutParams(itemParams);
+                            layout.addView(commits);
+                        }
 
-                if (pluginsettings.getBool("prs", true)) {
-                    TextView prs = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
-                    prs.setText("Total PRs: " + stats.get(2));
-                    prs.setLayoutParams(itemParams);
-                    layout.addView(prs);
-                }
+                        if (pluginsettings.getBool("prs", true)) {
+                            TextView prs = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
+                            prs.setText("Total PRs: " + stats.get(2));
+                            prs.setLayoutParams(itemParams);
+                            layout.addView(prs);
+                        }
 
-                if (pluginsettings.getBool("issues", true)) {
-                    TextView issues = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
-                    issues.setText("Total Issues: " + stats.get(3));
-                    issues.setLayoutParams(itemParams);
-                    layout.addView(issues);
-                }
+                        if (pluginsettings.getBool("issues", true)) {
+                            TextView issues = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
+                            issues.setText("Total Issues: " + stats.get(3));
+                            issues.setLayoutParams(itemParams);
+                            layout.addView(issues);
+                        }
 
-                if (pluginsettings.getBool("contributions", true)) {
-                    TextView contributions = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
-                    contributions.setText("Contributed to: " + stats.get(4));
-                    contributions.setLayoutParams(itemParams);
-                    layout.addView(contributions);
-                }
+                        if (pluginsettings.getBool("contributions", true)) {
+                            TextView contributions = new TextView(ctx, null, 0, R.i.UiKit_TextAppearance_Semibold);
+                            contributions.setText("Contributed to: " + stats.get(4));
+                            contributions.setLayoutParams(itemParams);
+                            layout.addView(contributions);
+                        }
 
-                // Add view if any one setting is trues
-                for (String key :
-                        pluginsettings.getAllKeys()) {
-                    if (pluginsettings.getBool(key, true)) {
-                        sheetContent.addView(layout, sheetContent.indexOfChild(sheetContent.findViewById(Utils.getResId("rich_presence_container", "id"))));
-                        break;
+                        // Add view if any one setting is trues
+                        for (String key :
+                                pluginsettings.getAllKeys()) {
+                            if (pluginsettings.getBool(key, true)) {
+                                sheetContent.addView(layout, sheetContent.indexOfChild(sheetContent.findViewById(Utils.getResId("rich_presence_container", "id"))));
+                                break;
+                            }
+                        }
+
+
+                    } catch (Exception e) {
+                        logger.error(e);
                     }
                 }
-
-
-            } catch (Exception e) {
-                logger.error(e);
             }
         }));
     }
@@ -152,7 +153,11 @@ public class GitHubStats extends Plugin {
         AtomicReference<String> rawHTML = new AtomicReference<>();
         var thread = new Thread(() -> {
             try {
-                rawHTML.set(Http.simpleGet("https://github-readme-stats.vercel.app/api?username=" + username));
+                rawHTML.set(
+                        Http.simpleGet(
+                                String.format(
+                                        "https://github-readme-stats.vercel.app/api?username=%s&unused=%s",
+                                        username, UUID.randomUUID().toString())));
             } catch (Exception e) {
                 logger.error(e);
             }
